39094b7bface3dca8803c0edd7e34c12dc82bd85
scottmwilson <scott@backstageconsult.com>
2023-02-15 15:05:57 -0500

    commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var Sent1 = "This is the begining of the string"; //This alerts the begining of my string
+
+var Family = "The Flinstones", Dad ="Fred", Mom = "Betty", Daughter = "Pebbles", Son = "Bamm-Bamm"; //defining the variables
+
+document.write(Son); //write son name 
+
+document.write("<br>");
+
+var Equation = (3 + 3); //this is the equation data
+
+var A = "is" + Equation + "years old"; //this is combining two words together
+
+document.write(A); //this is the request to write A
+
+var Sent2 = "and this is the end of the string"; //This alerts the end of my string
+
+document.write("<br>");
+
+document.write(Sent1 + Sent2);
