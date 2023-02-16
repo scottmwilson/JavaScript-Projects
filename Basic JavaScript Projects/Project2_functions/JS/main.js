e5a99d3339cbe04ed72c88d903d40e23fc28b9cc
scottmwilson <scott@backstageconsult.com>
2023-02-16 17:47:32 -0500

    commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //this is defining the function
+    var sentence = "I am having fun"; // this is my variable
+    sentence += " a lot of fun, fun, fun!"; // this is my operator
+    document.getElementById("Concatenate").innerHTML = sentence; // this is my concatenation 
+}
