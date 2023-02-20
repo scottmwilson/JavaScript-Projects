7eeb2a49f06adc79f73c39e98776dcc74c5e8782
scottmwilson <scott@backstageconsult.com>
2023-02-20 14:59:23 -0500

    commit

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,31 @@
-function () {
-    window.alert(Math.random() *100);
+function substraction_Function() { //this is my function
+    var Subtraction = 5 - 2; //this is my variable
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction; //this finds the ID the executes the equation
+}
+
+function addition_Function() { //this is my function
+    var addition = 5 + 2; //this is my variable
+    document.getElementById("One").innerHTML = "5 + 2 = " + addition; //this finds the ID the executes the equatio 
+}
+
+function multiplication_Function() { //this is my function
+    var multiplication = 5 * 2; //this is my variable
+    document.getElementById("Two").innerHTML = "5 * 2 = " + multiplication; //this finds the ID the executes the equatio 
+}
+
+function modulus_Function() { //this is my function
+    var modulus = 43 % 5; //this is my variable
+    document.getElementById("Three").innerHTML = "43 % 5 = " + modulus; //this finds the ID the executes the equatio
+}
+
+function increment_Function() { //this is my function
+    var X = 5; //this is my variable
+    X++; //this is adding 1 to my variable 
+    document.write(X); //this is requesting to write my variable
+}
+
+function decrement_Function() { //this is my function
+    var Y = 5.25; //this is my variable
+    Y--; //this is subtracting 1 to my variable 
+    document.write(Y); //this is requesting to write my variable 
 }
