2b6b312d958609bd5bcd42e1789a5ea4060dfe88
scottmwilson <scott@backstageconsult.com>
2023-02-20 15:07:47 -0500

    commit

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -6,5 +6,6 @@ function my_Dictionary() {
         Age: 5,
         Sound: "Bark!"
     };
-    document.getElementById("my_Dictionary").innerHTML.HTML = Animal.Sound;
+    delete Animal.Sound;
+    document.getElementById("my_Dictionary").innerHTML = Animal.Sound;
 } 
