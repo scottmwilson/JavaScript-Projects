4f7765c52931384088b475dcf8521fde26ae1645
scottmwilson <scott@backstageconsult.com>
2023-02-24 17:44:25 -0500

    commit

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -67,6 +67,6 @@ function text_length() {
         for (let i = 0; i < 10; i++) {
         if (i === 3) { break; }
         if (i === 3) { continue; }
-        text += "The number is " + i + "<br>";
-}
-document.getElementById("Sarah").innerHTML = text;
+        text += "The number is " + i + "<br>";  
+    }
+    document.getElementById("Sarah").innerHTML = text;
